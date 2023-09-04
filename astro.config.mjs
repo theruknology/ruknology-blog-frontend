a7e33f6217b7cbc7c40413987af5a28467bd9c1d
Mohammed Ruknuddin <iosrukn123@gmail.com>
2023-09-04 23:41:46 +0300

    Sanity Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sanity({
+    projectId: 'xyz',
+    dataset: 'production',
+    apiVersion: '2023-02-08',
+    useCdn: false,
+  })]
 });
